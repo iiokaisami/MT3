0c10045a412737c3bb6d7f92ef3a7af979097703
iiokaisami <k023g0025@g.neec.ac.jp>
2024-04-19 11:39:57 +0900

    MT3_0_5_確認課題

--- main.cpp
+++ main.cpp
@@ -1,12 +1,118 @@
 #include <Novice.h>
+#include <cmath>
+#include "Vector3.h"
+#include "assert.h"
 
-const char kWindowTitle[] = "LC1A_01_イイオカ_イサミ_MT3";
+struct Matrix4x4
+{
+	float m[4][4];
+};
+
+//X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian)
+{
+	Matrix4x4 result;
+	result = {
+		1.0f,0.0f,0.0f,0.0f,
+		0.0f,std::cos(radian),std::sin(radian),0.0f,
+		0.0f,-std::sin(radian),std::cos(radian),0.0f,
+		0.0f,0.0f,0.0f,1.0f,
+	};
+
+	return result;
+}
+
+//Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian)
+{
+	Matrix4x4 result;
+	result = {
+		std::cos(radian),0.0f,-std::sin(radian),0.0f,
+		0.0f,1.0f,0.0f,0.0f,
+		std::sin(radian),0.0f,std::cos(radian),0.0f,
+		0.0f,0.0f,0.0f,1.0f,
+	};
+
+	return result;
+}
+
+//Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian)
+{
+	Matrix4x4 result;
+	result = {
+		std::cos(radian),std::sin(radian),0.0f,0.0f,
+		-std::sin(radian),std::cos(radian),0.0f,0.0f,
+		0.0f,0.0f,1.0f,0.0f,
+		0.0f,0.0f,0.0f,1.0f,
+	};
+
+	return result;
+}
+
+
+//行列の積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2)
+{
+	Matrix4x4 result;
+
+	for (int row = 0; row < 4; row++)
+	{
+		for (int column = 0; column < 4; column++)
+		{
+			result.m[row][column] = m1.m[row][0] * m2.m[0][column] + m1.m[row][1] * m2.m[1][column] + m1.m[row][2] * m2.m[2][column] + m1.m[row][3] * m2.m[3][column];
+		}
+	}
+	return result;
+}
+
+//3次元のアフィン変換行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate)
+{
+	Matrix4x4 result;
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));//R
+
+	result = {
+		scale.x * rotateXYZMatrix.m[0][0],scale.x * rotateXYZMatrix.m[0][1], scale.x * rotateXYZMatrix.m[0][2],0.0f,
+	    scale.y * rotateXYZMatrix.m[1][0],scale.y * rotateXYZMatrix.m[1][1], scale.y * rotateXYZMatrix.m[1][2],0.0f,
+	    scale.z * rotateXYZMatrix.m[2][0],scale.z * rotateXYZMatrix.m[2][1], scale.z * rotateXYZMatrix.m[2][2],0.0f,
+	    translate.x,translate.y,translate.z,1.0f
+	};
+
+	return result;
+}
+
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix)
+{
+	for (int row = 0; row < 4; ++row)
+	{
+		for (int column = 0; column < 4; ++column)
+		{
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
+
+const char kWindowTitle[] = "LC1A_01_イイオカ_イサミ_MT3_0_5_確認課題";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Vector3 scale{ 1.2f,0.79f,-2.1f };
+	Vector3 rotate{ 0.4f,1.43f,-0.8f };
+	Vector3 translate{ 2.7f,-4.15f,1.57f };
+	Matrix4x4 worldMatrix = MakeAffineMatrix(scale, rotate, translate);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -32,6 +138,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatrixScreenPrintf(0, 0, worldMatrix);
 
 		///
 		/// ↑描画処理ここまで
