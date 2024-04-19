0adbda5f825f29e6e1b316a597f4c4c28532004d
iiokaisami <k023g0025@g.neec.ac.jp>
2024-04-19 12:39:32 +0900

    MT3_1_0_確認課題

--- main.cpp
+++ main.cpp
@@ -1,12 +1,81 @@
 #include <Novice.h>
+#include <cmath>
+#include "Vector3.h"
 
-const char kWindowTitle[] = "LC1A_01_イイオカ_イサミ_MT3";
+struct Matrix4x4
+{
+	float m[4][4];
+};
+
+//1. 透視投影行列
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip)
+{
+	Matrix4x4 result;
+	result = {
+		1 / aspectRatio * (1 / std::tan(fovY / 2)),0.0f,0.0f,0.0f,
+		0.0f,1 / std::tan(fovY / 2),0.0f,0.0f,
+		0.0f,0.0f,farClip / (farClip - nearClip),1.0f,
+		0.0f,0.0f,-nearClip * farClip / (farClip - nearClip),0.0f,
+	};
+
+	return result;
+}
+
+//2. 正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip)
+{
+	Matrix4x4 result;
+	result = {
+		2 / (right - left),0.0f,0.0f,0.0f,
+		0.0f,2 / (top - bottom),0.0f,0.0f,
+		0.0f,0.0f,1 / (farClip - nearClip),0.0f,
+		(left + right) / (left - right),(top + bottom) / (bottom - top),nearClip / (nearClip - farClip),1.0f,
+	};
+
+	return result;
+}
+
+//3. ビューポート変換行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth)
+{
+	Matrix4x4 result;
+	result = {
+		width / 2,0.0f,0.0f,0.0f,
+		0.0f,-height / 2,0.0f,0.0f,
+		0.0f,0.0f,maxDepth - minDepth,0.0f,
+		left + (width / 2),top + (height / 2),minDepth,1.0f
+	};
+
+	return result;
+}
+
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix)
+{
+	for (int row = 0; row < 4; ++row)
+	{
+		for (int column = 0; column < 4; ++column)
+		{
+			Novice::ScreenPrintf(x + column * kColumnWidth, y + row * kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
+
+const char kWindowTitle[] = "LC1A_01_イイオカ_イサミ_MT3_1_0_確認課題";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
+
+	Matrix4x4 orthographicMatrix = MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
+	Matrix4x4 perspectiveFovFovMatrix = MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+	Matrix4x4 viewportMatrix = MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -32,6 +101,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		MatrixScreenPrintf(0, 0, orthographicMatrix);
+		MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovFovMatrix);
+		MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix);
 
 		///
 		/// ↑描画処理ここまで
