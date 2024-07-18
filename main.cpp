219540f7397fb7db114f88c864e4c061fbee52fb
iiokaisami <k023g0025@g.neec.ac.jp>
2024-07-18 22:05:29 +0900

    MT3_4_0_応用課題

--- main.cpp
+++ main.cpp
@@ -1085,7 +1085,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		ImGui::DragFloat3("CameraRotate", &cameraRotate.x, 0.01f);
 
 		ImGui::Text("setting");
-		
 		if (ImGui::Button("start"))
 		{
 			isStart = true;
